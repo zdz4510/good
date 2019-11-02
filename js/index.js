0c63de6e1a2148a90c92bd17537d75b55b67a26d
zdz <2513408243@qq.com>
2019-11-02 11:15:48 +0800

    添加正则表达式对手机号进行限制

--- js/index.js
+++ js/index.js
@@ -6,8 +6,8 @@ $(function(){
 		$(this).siblings().removeClass('current');
 		if($(this).text() == '顾客管理'){
 			$('.wrapper').load('./pages/customer.html');
-		}else if($(this).text() == '员工管理'){
-			$('.wrapper').load('./pages/waiter.html');
+		}else if($(this).text() == '订单管理'){
+			$('.wrapper').load('./pages/order.html');
 		}else if($(this).text() == '栏目管理'){
 			$('.wrapper').load('./pages/category.html');
 		}else if($(this).text() == '地址管理'){
